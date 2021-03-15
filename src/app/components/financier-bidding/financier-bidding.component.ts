ec3030cef46a2f66faaff4e3b12d8208c65a9954
Gisjegadeesh <jegadeesh.b@greatinnovus.com>
2021-03-15 17:22:41 +0530

    March -1 Side bar, Alighnments, Dropdowns

--- src/app/components/financier-bidding/financier-bidding.component.ts
+++ src/app/components/financier-bidding/financier-bidding.component.ts
@@ -22,6 +22,62 @@ const ELEMENT_DATA: any[] = [
     Email: ''
   },
 ];
+const DATA_ONE: any[] = [
+  {
+    SNo: 1,
+    DescGoods: 'Steel Rod',
+    IdNo: 'a456',
+    Qty: '100t',
+    Rate: '678.0',
+    Amt: 67800,
+    DiscAmt: '-',
+    NetAmt: 67800,
+    TaxRate: 2,
+    TaxAmt: 1356,
+    Total: 63156
+  },
+  {
+    SNo: 2,
+    DescGoods: 'Nuts',
+    IdNo: 'D435',
+    Qty: '25t',
+    Rate: '876.0',
+    Amt: 21900,
+    DiscAmt: 900,
+    NetAmt: 21000,
+    TaxRate: 2,
+    TaxAmt: 420,
+    Total: 21420
+  }
+];
+const DATA_TWO: any[] = [
+  {
+    BidID: 'BID03456',
+    FinOffAmt: 102700,
+    Ccy: 'SGD',
+    FxRateDiff: '1.35',
+    Margin: 10,
+    DiscRate: 3,
+    DiscAmt: 760,
+    NetAmtPay: 101940,
+    DueDate: '90D/10Mar21',
+    OffExpPrd: '4 PM',
+    Status: 'A',
+  },
+  {
+    BidID: 'BID03456',
+    FinOffAmt: 102700,
+    Ccy: 'SGD',
+    FxRateDiff: '1.35',
+    Margin: 10,
+    DiscRate: 3,
+    DiscAmt: 760,
+    NetAmtPay: 101940,
+    DueDate: '90D/10Mar21',
+    OffExpPrd: '4 PM',
+    Status: 'A'
+  }
+];
 @Component({
   selector: 'app-financier-bidding',
   templateUrl: './financier-bidding.component.html',
@@ -78,7 +134,35 @@ export class FinancierBiddingComponent implements OnInit {
     }
   }
   constructor(public router: Router,private modalService: BsModalService,private modalDialogService:ModalDialogService,private authenticationService: AuthenticationService) { }
- 
+  dataSourceOne = new MatTableDataSource(DATA_ONE); //data
+  dataSourceTwo = new MatTableDataSource(DATA_TWO); //data
+
+  displayedColumnsOne: string[] = [
+    'SNo',
+    'DescGoods',
+    'IdNo',
+    'Qty',
+    'Rate',
+    'Amt',
+    'DiscAmt',
+    'NetAmt',
+    'TaxRate',
+    'TaxAmt',
+    'Total'
+  ];
+  displayedColumnsTwo: string[] = [
+    'BidID',
+    'FinOffAmt',
+    'Ccy',
+    'FxRateDiff',
+    'Margin',
+    'DiscRate',
+    'DiscAmt',
+    'NetAmtPay',
+    'DueDate',
+    'OffExpPrd',
+    'Status'
+  ];
   ngOnInit() {
     if (window.innerWidth < 415) {
       this.mobileScreen = true;
